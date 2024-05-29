4551686a763a13ec0010af14ba461b0f115d68a0
jonnystalnaker <steamsynthetic@gmail.com>
2024-05-29 13:12:17 -0700

    creating question timer and using useEffect

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,22 +1,43 @@
 ﻿import { useState } from 'react';
+import quizCompleteImg from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions.js';
+import QuestionTimer from './QuestionTimer.jsx';
 
 export default function Quiz() {
 	const [userAnswers, setUserAnswers] = useState([]);
 	const activeQuestionIndex = userAnswers.length;
+	const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
 	function handleSelectAnswer(selectedAnswer) {
 		setUserAnswers(prevUserAnswers => {
 			return [...prevUserAnswers, selectedAnswer];
 		});
 	}
+	if (quizIsComplete) {
+		return (
+			<div id="summary">
+				<img
+					src={quizCompleteImg}
+					alt="Trophy Icon"
+				/>
+				<h2>Quiz Completed</h2>
+			</div>
+		);
+	}
+
+	const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+	shuffledAnswers.sort(() => Math.random() - 0.5);
 
 	return (
 		<div id="quiz">
 			<div id="question">
+				<QuestionTimer
+					timeout={10000}
+					onTimeout={() => handleSelectAnswer(null)}
+				/>
 				<h2>{QUESTIONS[activeQuestionIndex.text]}</h2>
 				<ul id="answers">
-					{QUESTIONS[activeQuestionIndex].answers.map(answer => (
+					{shuffledAnswers.map(answer => (
 						<li
 							key={answer}
 							className="answer"
